cb7ddb7f78b71310be305475f591f79dbe8044d5
Rozhkov109 <123330362+Rozhkov109@users.noreply.github.com>
2023-11-30 12:55:54 +0100

    Добавьте файлы проекта.

---
+++ Курсова C# 3 Семестр/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Курсова_C__3_Семестр
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
