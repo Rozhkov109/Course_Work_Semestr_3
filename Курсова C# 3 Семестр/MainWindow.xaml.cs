71e21c654309848c41e02f1daf41a16a20bee1c1
Rozhkov109 <123330362+Rozhkov109@users.noreply.github.com>
2023-12-08 22:35:53 +0100

    Создан каркас приложения.
    Добавлены кнопки, таблицы и меню.

--- Курсова C# 3 Семестр/MainWindow.xaml.cs
+++ Курсова C# 3 Семестр/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace CourseWork
 {
@@ -7,6 +9,67 @@ namespace CourseWork
         public MainWindow()
         {
             InitializeComponent();
+            ErrorManager.ErrorHandler = (errorMessage) => ErrorManager.GetErrorMessage(errorMessage);
+        }
+
+        private void FxAddPoint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FxRemovePoint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GxAddPoint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EnterNumbersToTextBox(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox != null)
+            {
+                string newText = textBox.Text;
+                if (!string.IsNullOrEmpty(newText) && newText.All(c => char.IsDigit(c) || c == '.' || c == '-'))
+                {
+                    // Дозволяємо запис
+                }
+                else
+                {
+                    // Не дозволяємо запис
+                    textBox.Text = string.Concat(newText.Where(c => char.IsDigit(c) || c == '.' || c == '-'));
+                    textBox.CaretIndex = textBox.Text.Length; // установить каретку в конец текста
+                }
+            }
+        }
+
+        private void EnterNumbersToEpsTextBox(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox != null)
+            {
+                string newText = textBox.Text;
+                if (!string.IsNullOrEmpty(newText) && newText.All(c => char.IsDigit(c) || c == '.'))
+                {
+                    // Дозволяємо запис
+                }
+                else
+                {
+                    // Не дозволяємо запис
+                    textBox.Text = string.Concat(newText.Where(c => char.IsDigit(c) || c == '.'));
+                    textBox.CaretIndex = textBox.Text.Length; // установить каретку в конец текста
+                }
+            }
+        }
+
+        private void FindMaximumButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- Курсова C# 3 Семестр/Properties/Resources.Designer.cs
+++ Курсова C# 3 Семестр/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Курсова_C__3_Семестр.Properties {
+namespace CourseWork.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Курсова_C__3_Семестр.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Курсова_C__3_Семестр.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CourseWork.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
